f2382441d966ce31b12eb29e6ba70bd13a16e08b
service-city-panama <163794663+service-city-panama@users.noreply.github.com>
2024-04-18 20:45:38 -0400

    Add files via upload

--- js/modal.js
+++ js/modal.js
@@ -1,27 +1,21 @@
+// Modal.js
 
-// ========== ========== Modal ========== ==========
 // Función para abrir un modal específico
 function abrirModal(id) {
-    document.getElementById(id).style.display = 'block';
-  }
-  
-  // Función para cerrar un modal específico
-  function cerrarModal(id) {
-    document.getElementById(id).style.display = 'none';
-  }
-  
-  // Eventos para los botones de abrir
-  document.getElementById('btnModal-signup').onclick = function() { abrirModal('modal-signup'); }
-  document.getElementById('btnModal-forgotPassword').onclick = function() { abrirModal('modal-forgotPassword'); }
-  // Secretarios
-  document.getElementById('btnModal-chapalin').onclick = function() { abrirModal('modal-chapalin'); }
-  document.getElementById('btnModal-gabriel').onclick = function() { abrirModal('modal-gabriel'); }
-  document.getElementById('btnModal-pollito').onclick = function() { abrirModal('modal-pollito'); }
-  
-  // Cerrar el modal si se hace clic fuera de él
-  window.onclick = function(event) {
-    if (event.target.className === 'modal') {
-      event.target.style.display = 'none';
-    }
-  }
-  // ========== ========== Modal ========== ==========
+  document.getElementById(id).style.display = "block";
+}
+
+// Función para cerrar un modal específico
+function cerrarModal(id) {
+  document.getElementById(id).style.display = "none";
+}
+// Secretarios
+document.getElementById("btnModal-chapalin").onclick = function () {
+  abrirModal("modal-chapalin");
+};
+document.getElementById("btnModal-gabriel").onclick = function () {
+  abrirModal("modal-gabriel");
+};
+document.getElementById("btnModal-pollito").onclick = function () {
+  abrirModal("modal-pollito");
+};
