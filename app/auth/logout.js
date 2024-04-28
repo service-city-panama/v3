df8d02350402c14de1bcefe6d7707a010201a7cb
service-city-panama <163794663+service-city-panama@users.noreply.github.com>
2024-04-28 18:18:00 -0400

    Delete app directory

--- app/auth/logout.js
+++
@@ -1,9 +0,0 @@
-import { signOut } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
-import { auth } from "../firebase.js";
-
-const logout = document.querySelector('#logout')
-
-logout.addEventListener('click', async () => {
-    await signOut(auth)
-    console.log('user signed out')
-})
